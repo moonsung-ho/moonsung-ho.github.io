4c5705fd8171cefc5f8f041a737ad11161f5e4d0
Sungho Moon <me@sungho.xyz>
2024-04-12 08:19:46 +0900

    head.js 업데이트

--- app/head.js
+++ app/head.js
@@ -4,6 +4,8 @@ import Script from "next/script";
 export default function Head() {
   return (
     <>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7245930610023842"
+     crossorigin="anonymous"></script>
       <title>문성호</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="cemo" />
